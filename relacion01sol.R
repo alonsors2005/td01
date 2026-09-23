111e8f8528b21d816d699acbccc1aefc92638263
alonsors2005 <alonsors2005@gmail.com>
2026-09-23 17:21:24 +0200

    actualizado a sol problema 1 a

--- relacion01sol.R
+++ relacion01sol.R
@@ -1,7 +1,12 @@
 
 #Problema 1
+source("teoriadecision_funciones_incertidumbre.R")
+##1 a)
+dat1a=crea.tablaX(c(5,4,6,2,3,1,-1,8,7,5,2,0),numalternativas = 4,numestados = 3)
+res1=criterio.Todos(dat1a,alfa=0.5,favorable = T)
+res1
 
-#Problema 2u u4rb
+#Problema 2u 
 
 media=function(x){
   returun(sum(x)/length(x))
